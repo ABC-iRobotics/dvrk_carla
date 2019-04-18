85e7d9351346b5cea0701de04cdbccd3d43403b8
Tamas D. Nagy <tamas.daniel.nagy@irob.uni-obuda.hu>
2019-04-18 13:35:59 +0200

    Orienatation fixed

--- scripts/dvrk_steering_wheel.py
+++ scripts/dvrk_steering_wheel.py
@@ -46,12 +46,12 @@ class dvrk_steering_wheel:
         self.coag_event.wait(600)
         
     def quaternion_multiply(self, quaternion1, quaternion0):
-        w0, x0, y0, z0 = quaternion0
-        w1, x1, y1, z1 = quaternion1
-        return numpy.array([-x1 * x0 - y1 * y0 - z1 * z0 + w1 * w0,
-                     x1 * w0 + y1 * z0 - z1 * y0 + w1 * x0,
+        x0, y0, z0, w0  = quaternion0
+        x1, y1, z1,w1 = quaternion1
+        return numpy.array([x1 * w0 + y1 * z0 - z1 * y0 + w1 * x0,
                      -x1 * z0 + y1 * w0 + z1 * x0 + w1 * y0,
-                     x1 * y0 - y1 * x0 + z1 * w0 + w1 * z0], dtype=numpy.float64)
+                     x1 * y0 - y1 * x0 + z1 * w0 + w1 * z0,
+                     -x1 * x0 - y1 * y0 - z1 * z0 + w1 * w0], dtype=numpy.float64)
 
 
     def do_wheel_things(self):
@@ -84,43 +84,44 @@ class dvrk_steering_wheel:
         center_l = numpy.array([center_r[0] + x_offset, center_r[1], center_r[2]])
         
         couple_f = 20.0
-
+        #couple_f = 0.0
+        
         wheel_multiplier = 1.0 / 180.0
 
         gains_l.PosStiffNeg.y = -400.0;
         gains_l.PosStiffPos.y = -400.0;
-        gains_l.PosDampingNeg.y = -10.0;
-        gains_l.PosDampingPos.y = -10.0;
+        gains_l.PosDampingNeg.y = -3.0;
+        gains_l.PosDampingPos.y = -3.0;
 
         # Tangential direction
         gains_l.PosStiffNeg.z = -1.0;
         gains_l.PosStiffPos.z = -1.0;
-        gains_l.PosDampingNeg.z = -3.0;
-        gains_l.PosDampingPos.z = -3.0;
+        gains_l.PosDampingNeg.z = -2.0;
+        gains_l.PosDampingPos.z = -2.0;
 
         gains_l.PosStiffNeg.x = -400.0;
         gains_l.PosStiffPos.x = -400.0;
-        gains_l.PosDampingNeg.x = -10.0;
-        gains_l.PosDampingPos.x = -10.0;
+        gains_l.PosDampingNeg.x = -3.0;
+        gains_l.PosDampingPos.x = -3.0;
         
         gains_r.PosStiffNeg.y = -400.0;
         gains_r.PosStiffPos.y = -400.0;
-        gains_r.PosDampingNeg.y = -2.0;
-        gains_r.PosDampingPos.y = -2.0;
+        gains_r.PosDampingNeg.y = -1.0;
+        gains_r.PosDampingPos.y = -1.0;
 
         # Tangential direction
         gains_r.PosStiffNeg.z = -1.0;
         gains_r.PosStiffPos.z = -1.0;
-        gains_r.PosDampingNeg.z = -3.0;
-        gains_r.PosDampingPos.z = -3.0;
+        gains_r.PosDampingNeg.z = -2.0;
+        gains_r.PosDampingPos.z = -2.0;
 
         gains_r.PosStiffNeg.x = -400.0;
         gains_r.PosStiffPos.x = -400.0;
-        gains_r.PosDampingNeg.x = -10.0;
-        gains_r.PosDampingPos.x = -10.0;
+        gains_r.PosDampingNeg.x = -3.0;
+        gains_r.PosDampingPos.x = -3.0;
 
-        stiffOri = -1;
-        dampOri = -0.01;
+        stiffOri = -0.1;
+        dampOri = -0.001;
         gains_l.OriStiffNeg.x = stiffOri;
         gains_l.OriStiffPos.x = stiffOri;
         gains_l.OriDampingNeg.x = dampOri;
@@ -153,7 +154,8 @@ class dvrk_steering_wheel:
         gains_r.ForcePosition.y = self.arm_r.get_current_position().p[1]
 
         rate = rospy.Rate(100) # 10hz
-        i = 0
+        self.arm_l.unlock_orientation()
+        self.arm_r.unlock_orientation()
         while not rospy.is_shutdown():
           #print rospy.get_caller_id(), ' -> Refresh gains'
           currpos_l = self.arm_l.get_current_position()
@@ -187,8 +189,17 @@ class dvrk_steering_wheel:
           steer_ori_r = numpy.array([0.0, numpy.cos(steer_angle_r/2.0), 0.0, numpy.sin(steer_angle_r/2.0)])
           steer_ori_l = numpy.array([0.0, numpy.cos(steer_angle_l/2.0), 0.0, numpy.sin(steer_angle_l/2.0)])
           
-          torque_ori_r = self.quaternion_multiply(base_ori_r, steer_ori_r)
-          torque_ori_l = self.quaternion_multiply(base_ori_l, steer_ori_l)
+          end_rot_r = -1.0*numpy.pi
+          
+          end_rot_l = 0.0*numpy.pi
+          
+          steer_ori_rot_r = numpy.array([0.0, numpy.cos((steer_angle_r + end_rot_r)/2.0), 0.0, numpy.sin((steer_angle_r + end_rot_r)/2.0)])
+          steer_ori_rot_l = numpy.array([0.0, numpy.cos((steer_angle_l + end_rot_l)/2.0), 0.0, numpy.sin((steer_angle_l + end_rot_l)/2.0)])
+          
+          torque_ori_r = self.quaternion_multiply(steer_ori_rot_r, base_ori_r)
+          
+          
+          torque_ori_l = self.quaternion_multiply(steer_ori_rot_l, base_ori_l)
     
           gains_l.TorqueOrientation.x = torque_ori_l[0]
           gains_l.TorqueOrientation.y = torque_ori_l[1]
@@ -224,17 +235,16 @@ class dvrk_steering_wheel:
             gains_r.ForceBiasPos.z = -couple_f * angle_diff
             gains_r.ForceBiasNeg.z = -couple_f * angle_diff
 
-          if i==0:
-            self.set_gains_l_pub.publish(gains_l)
-            self.set_gains_r_pub.publish(gains_r)
-            i = i + 1
+
+          self.set_gains_l_pub.publish(gains_l)
+          self.set_gains_r_pub.publish(gains_r)
+
 
           wheel_msg = Float32()
           wheel_msg.data = numpy.rad2deg(steer_angle_l_rel) * wheel_multiplier
           self.wheel_pub.publish(wheel_msg)
           
-          self.arm_l.unlock_orientation()
-          self.arm_r.unlock_orientation()
+          
           rate.sleep()
 
 
